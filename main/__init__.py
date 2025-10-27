a99fc575baf76fb824bfa340449fb1cc66942895
Amirhossein bakhshaei <amirbakhshaei@gmail.com>
2025-10-27 20:46:52 +0330

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -19,10 +19,12 @@ SESSION = config("SESSION", default=None)
 FORCESUB = config("FORCESUB", default=None)
 AUTH = config("AUTH", default=None, cast=int)
 
-bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
-
-userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
-
+if not SESSION:
+    # Use the Bot Token (only if no session is provided)
+    bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+else:
+    # Use the Userbot (session string)
+    userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID)
 try:
     userbot.start()
 except BaseException:
